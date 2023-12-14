2a54e587a2efa7d96d8ade8d04f2500df9848760
Esteban <baab2an@live.fr>
2023-12-14 20:40:16 +0100

    FEAT création d'un router

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className='footer'>
         <div className='footer__container'>
-            <img src='logofooter1.png' alt='logo'/>
+            <img className='footer__logo' src='logofooter1.png' alt='logo'/>
         </div>
         <div className='footer__text'>
         © 2020 Kasa. All rights reserved
--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter, Router, RouterProvider, } from "react-router-dom";
+
+const router = createBrowserRouter([
+  {
+  path: "/",
+  element: <App />
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
