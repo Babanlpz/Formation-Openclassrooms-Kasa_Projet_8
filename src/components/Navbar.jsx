af1333580a569d6c6426fc22b81155f0064720e3
Esteban <baab2an@live.fr>
2023-12-14 21:26:35 +0100

    FEAT NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
-import { Link } from "react-router-dom"
+import { NavLink } from "react-router-dom"
 import "./Navbar.css"
 
 function Navbar() {
+
+  let activeStyle = {
+    textDecoration: " underline",
+  }
+
+  let activeClasseName = "underline"
+
   return (
     <nav className='navbar'>
     <div className='navbar__logo'>
         <img src='logo.png' alt='Logo' />
     </div>
-    <Link to="/">
-      <div>Acceuil</div>
-    </Link>
-    <Link to= "/about">
-      <div>A propos</div>
-    </Link>
+    <NavLink to="/" style={({ isActive }) => 
+      isActive ? activeStyle : undefined
+  }>
+      Acceuil
+    </NavLink>
+    <NavLink to= "/about" className={({ isActive}) => 
+      isActive ? activeClasseName : undefined
+  }>
+      A propos
+    </NavLink>
 
   </nav>
     
