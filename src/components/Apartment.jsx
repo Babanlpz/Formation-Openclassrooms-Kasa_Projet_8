026cccdb346b180e013070ddd7ed4a697810c375
Esteban <baab2an@live.fr>
2023-12-19 23:27:56 +0100

    FEAT importation dbjson

--- src/components/Apartment.jsx
+++ src/components/Apartment.jsx
@@ -2,13 +2,17 @@ import React from 'react'
 import "./Apartment.scss"
 import { Link } from "react-router-dom"
 
-function Apartment() {
+function Apartment(props) {
+  console.log("props dans apartment:", props)
   return (
-    <div className='apartment'>
+    
       <Link to="/flat">
-          <div className='apartment__subtitle'>Titre de la location</div>
+          <div className='apartment'>
+            <img src={props.imageUrl} alt=''/>
+          <div className='apartment__subtitle'>{props.title}</div> 
+         </div>
       </Link>
-    </div>
+  
   )
 }
 
--- src/components/ApartmentsGrid.jsx
+++ src/components/ApartmentsGrid.jsx
@@ -1,16 +1,26 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import "./ApartmentsGrid.scss"
 import Apartment from './Apartment.jsx'
 
+
 function ApartmentsGrid() {
+  const [apartments, setApartments] = useState([])
+
+  useEffect(fetchApartments, [])
+
+  function fetchApartments() {
+     fetch("db.json")
+    .then((res) => res.json())
+    .then((res) => setApartments(res))
+    .catch(console.error); 
+  }
+
   return (
     <div className='grid'>
-        <Apartment />
-        <Apartment />
-        <Apartment />
-        <Apartment />
-        <Apartment />
-        <Apartment />
+      {apartments.map((apartment) => (
+          <Apartment title={apartment.title} imageUrl={apartment.cover}/>
+       ))}
+      
     </div>
   )
 }
