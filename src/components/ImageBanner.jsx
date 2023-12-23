b1eab694c6d38815f4bf12ef195ecf7bc75ad8e2
Esteban <baab2an@live.fr>
2023-12-23 10:45:54 +0100

    FEAT banner layout about

--- src/components/ImageBanner.jsx
+++ src/components/ImageBanner.jsx
@@ -25,20 +25,35 @@ const moveToPrevious = () => {
   }
   setCurrentPicture((currentPicture - 1) % pictures.length)
 }
+
+const arePicturesAvailable = () => {
+  return pictures && pictures.length > 0
+}
+
+const getCarouselOrDefaultImage = () => {
+  if (!arePicturesAvailable()) {
+    return <img src="image3.png" alt="" className='show' />
+  }
+    return pictures.map((pic, i) => (
+    <img key={pic} src={pic} alt="" className={getClassName(i)}></img>
+  ))
+}
+
   return (
     <div className='image__banner'> 
    
       <div className='image__container'>
-       {/*<img src={imageUrl} alt='Image appartement' />*/}
-       { pictures.map((pic, i) => <img 
-       key={pic} src={pic} alt="" className={ getClassName(i)}></img>)}
-       </div>
+          {getCarouselOrDefaultImage()}
+      </div>
+        {arePicturesAvailable() &&
+        <>
         <button className="btn btn-next" onClick={moveToNext}>
         <i class="fas fa-chevron-right"></i>
         </button>
         <button className="btn btn-previous" onClick={moveToPrevious}>
         <i class="fas fa-chevron-left"></i>
         </button>
+        </>}
     </div>
   )
 }
